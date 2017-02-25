d408acaf179dab36f3e6584afe4606febeb912b6
Aurélien Gâteau <mail@agateau.com>
2017-02-25 14:12:00 +0100

    Use a separate url to add a quote

--- views.py
+++ views.py
@@ -1,15 +1,17 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 
 from app import app
 from models import Quote
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def homepage():
-    if request.method == 'POST':
-        if request.form.get('content'):
-            # Create a new quote in the db.
-            Quote.create(content=request.form['content'])
-
     quotes = Quote.public().limit(50)
     return render_template('homepage.html', quotes=quotes)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.form.get('content'):
+        Quote.create(content=request.form['content'])
+    return redirect('/')
