3529ebfedf27aeae28245415d58e17bf8b1472c3
Aurélien Gâteau <mail@agateau.com>
2017-03-04 18:27:47 +0100

    Add an import tool

--- README.md
+++ README.md
@@ -4,6 +4,21 @@ Super minimalist quote gathering tool.
 
 Based on <http://charlesleifer.com/blog/saturday-morning-hack-a-little-note-taking-app-with-flask/>
 
+## quoteimport
+
+`quoteimport` lets you import quotes from a JSON file. The JSON file must have
+the following format:
+
+    {
+        "quotes": [
+            {
+                "date": "date-in-iso8601-format",
+                "text": "quote text"
+            },
+            //...
+        ]
+    }
+
 ## Static files sources
 
 <https://github.com/twbs/bootstrap/releases/download/v3.1.1/bootstrap-3.1.1-dist.zip>
---
+++ quoteimport.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import sys
+
+import arrow
+
+from models import Quote
+
+
+DESCRIPTION = """\
+Import quotes from a JSON file. See README.md for details of the format.
+"""
+
+
+def do_import(quotes):
+    for quote in quotes:
+        date = arrow.get(quote['date']).datetime.strftime('%Y-%m-%d %H:%M:%S')
+        text = quote['text']
+        Quote.create(date=date, content=text)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.description = DESCRIPTION
+
+    parser.add_argument('-c', '--clean', action='store_true',
+                        help='Delete all quotes before importing. Dangerous!')
+
+    parser.add_argument('json_file')
+
+    args = parser.parse_args()
+
+    with open(args.json_file) as f:
+        quotes = json.load(f)['quotes']
+
+    if args.clean:
+        Quote.delete().execute()
+    do_import(quotes)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+# vi: ts=4 sw=4 et
